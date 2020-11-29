4a4eb1be4ad0cd0e8fc98a8926a4bda918e5e59c
flexflower <kelyancsajka@icloud.com>
2020-11-29 20:52:21 +0100

    optimize reload countdown

--- javascript/relcountdown.js
+++ javascript/relcountdown.js
@@ -1,12 +1,10 @@
-var time = 1 * 5;
-setInterval(generateQR, time * 1000);
-var r = document.getElementById("reloadcount");
-var tmp = time;
-
-setInterval(function () {
-  var c = tmp--,
-    min = (c / 60) >> 0,
-    sec = c - min * 60 + "";
-  r.textContent = `Reload in  ${min}:${sec.length > 1 ? "" : "0"}${sec}`;
-  tmp != 0 || (tmp = time);
+var timeleft = 60;
+var countdown = setInterval(function () {
+  if (timeleft <= 0) {
+    location.reload();
+  } else {
+    document.getElementById("reloadcount").innerHTML =
+      "Reload in " + timeleft + "s";
+  }
+  timeleft -= 1;
 }, 1000);
